d8e3b8095ffe8a1a5ea3b21742933a7c19b97745
Dara Phin <dara.phin@student.cadt.edu.kh>
2024-07-19 21:25:08 +0700

    message_UI

--- lib/screen/pages/bottomNavPages/messages_tab.dart
+++ lib/screen/pages/bottomNavPages/messages_tab.dart
@@ -15,127 +15,131 @@ class _Chat2State extends State<Chat2> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 24, 23, 23),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.18,
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.65,
-                    child: const Center(
-                      child: Text(
-                        'Share ideas width your friends',
-                        maxLines: 2,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 26,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    height: 40,
-                    width: MediaQuery.of(context).size.width * 0.85,
-                    child: TextFormField(
-                      textAlignVertical: TextAlignVertical.bottom,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      decoration: const InputDecoration(
-                          hintText: 'Search by name or email address',
-                          hintStyle: TextStyle(color: Color.fromARGB(195, 255, 255, 255)),
-                          prefixIcon: Icon(
-                            Icons.search,
-                            color: Color.fromARGB(223, 255, 255, 255),
-                          ),
-                          filled: true,
-                          fillColor: Color.fromARGB(255, 54, 52, 52),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                            borderSide: BorderSide.none,
-                          )),
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              isActive == false
-                  ? chatPage2Widget(
-                      const Color.fromARGB(216, 243, 241, 241),
-                      'S',
-                      null,
-                      'Sync contacts',
-                      context,
-                      () {
-                        showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                backgroundColor: const Color.fromARGB(255, 43, 42, 42),
-                                title: const Text(
-                                  'So\'rov',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                content: const Text(
-                                  'Dasturga kirganda birmartta soraladigan sorov',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                                actions: <Widget>[
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                      textStyle: Theme.of(context).textTheme.labelLarge,
-                                    ),
-                                    child: const Text(
-                                      'No',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                  ),
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                      textStyle: Theme.of(context).textTheme.labelLarge,
-                                    ),
-                                    child: const Text(
-                                      'Yes',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                      isActive = true;
-                                      setState(() {});
-                                    },
-                                  ),
-                                ],
-                              );
-                            });
-                      },
-                    )
-                  : const SizedBox(
-                      width: 0.000000001,
-                    ),
-            ],
+      body: _buildBody(),
+    );
+  }
+
+  Column _buildBody() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(top: 15),
+          padding: const EdgeInsets.only(left: 5),
+          child: const Text(
+            'Message',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
           ),
         ),
-      ),
+        const ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 248, 202, 202),
+            child: Text(
+              "D",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 19,
+              ),
+            ),
+          ),
+          trailing: Text('7:54 AM',
+          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+          title: Text(
+            "Dom",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+          subtitle: Text(
+            "នៅណា",
+            style: TextStyle(
+              fontSize: 12,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+        ),
+        const ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 248, 202, 202),
+            child: Text(
+              "H",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 19,
+              ),
+            ),
+            
+          ),
+          trailing: Text('4:14 PM',
+          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+          title: Text(
+            "Va Neathh",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+          subtitle: Text(
+            "Hello",
+            style: TextStyle(
+              fontSize: 12,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+        ),
+        const ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 248, 202, 202),
+            child: Text(
+              "V",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 19,
+              ),
+            ),
+            
+          ),
+          trailing: Text('9:20 PM',
+          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+          ),
+          title: Text(
+            "Heng",
+            style: TextStyle(
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+          subtitle: Text(
+            "Hi",
+            style: TextStyle(
+              fontSize: 12,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 15),
+          padding: const EdgeInsets.only(left: 5),
+          child: const Text(
+            'Contacts',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
